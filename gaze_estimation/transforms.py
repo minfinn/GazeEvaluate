05de4304ad54f5e68e2ecf4ab0c7b2e3294573b3
finnson <finnson117@gmail.com>
2024-10-21 19:04:02 +0800

    change MPII transform

--- gaze_estimation/transforms.py
+++ gaze_estimation/transforms.py
@@ -43,15 +43,19 @@ def _create_mpiifacegaze_transform(config: yacs.config.CfgNode) -> Any:
         to_gray = identity
     if config.dataset.name == 'MPII':
         transform = torchvision.transforms.Compose([
-            resize,
+            # resize,
             to_gray,
-            torchvision.transforms.Lambda(lambda x: x.transpose(2, 0, 1)),
+            # torchvision.transforms.Lambda(lambda x: x.transpose(2, 0, 1)),
             scale,
             torch.from_numpy,
-            torchvision.transforms.Normalize(mean=[0.406, 0.456, 0.485],
-                                            std=[0.225, 0.224, 0.229]),
+            # torchvision.transforms.Normalize(mean=[0.406, 0.456, 0.485],
+            #                                 std=[0.225, 0.224, 0.229]),
+            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225]),
+
         ])
     elif config.dataset.name == 'GAZE360' :
+        #TODO(finn): change transform to ensure image format is [3, 224, 224] with RGB 
         transform = torchvision.transforms.Compose([
             resize,
             to_gray,
@@ -62,10 +66,10 @@ def _create_mpiifacegaze_transform(config: yacs.config.CfgNode) -> Any:
                                             std=[0.229, 0.224, 0.225]),
         ])
     elif config.dataset.name == 'XGAZE' :
-        transform = transforms.Compose([ # 测试数据集的转换操作
-            transforms.ToPILImage(),
-            transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        transform = torchvision.transforms.Compose([ # 测试数据集的转换操作
+            torchvision.transforms.ToPILImage(),
+            torchvision.transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
+            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225]),
         ])
     return transform
